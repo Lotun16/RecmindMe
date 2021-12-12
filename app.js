35f379a97caa92223cf2bdda2ffc7dd6fb67ab82
Lotun16 <38048224+Lotun16@users.noreply.github.com>
2021-12-12 17:04:20 -0500

    connected to mongoDB and initiated express

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 const express = require('express');
-console.log('hi');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+// console.log('hi');
+
+//express app
+const app = express();
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://lotun:wyckoff20@rec-mind.54sg6.mongodb.net/rec-mind?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log("connected to db");
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+//register view engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('public'));
+
+//middleware
+app.use(morgan('dev'));
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home'});
+});
